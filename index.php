a2bd7abb2c5f846c7e7ad4a00b4594767ac89d67
Lud972vic <e.ludovic@free.fr>
2021-06-02 17:57:49 +0200

    Inversement des méthodes addMessage puis findAll

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 
 require('model/model.php');
 
-//Les données de la table message
-$data = findAll();
-
-//La view
-require('view/default.php');
-
 //Vérifie que le formulaire est soumis 
 if (isset($_POST['submit'])) {
     //On ajout le message
     addMessage($_POST['pseudo_fe'], $_POST['message_fe']);
 }
+
+//Les données de la table message
+$data = findAll();
+
+//La view
+require('view/default.php');
